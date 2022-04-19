23d63f7f75f1d36f4720c2677899905f8582ddd9
gstyanti <101311934+gstyanti@users.noreply.github.com>
2022-04-19 17:49:36 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi_syifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan(190302124, "joni", 2000000);
+
+
+            Console.WriteLine("NIK \t Nama \t\t Gaji");
+            Console.WriteLine("-------------------------------");
+            karyawan.showKaryawan();
+            karyawan2.showKaryawan();
+
+            Console.WriteLine(" Asyiiiiik kenaikan gaji 10!!");
+
+            Console.WriteLine("NIK \t Nama \t\t Gaji");
+            Console.WriteLine("-------------------------------");
+            karyawan.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
